f089b7532d8769cf9a85a6cbda401087046052b2
Corchu22 <corchu2209@gmail.com>
2024-11-28 02:22:40 +0100

    Patrolling Goomba Hecho

--- Assets/NavegationScript.cs
+++ Assets/NavegationScript.cs
@@ -5,17 +5,43 @@ using UnityEngine.AI;
 
 public class NavegationScript : MonoBehaviour
 {
-    public Transform point;
+    public Transform m_point1;
+    public Transform m_point2;
+    public Transform m_point3;
+    public Transform m_point4;
+    public Transform m_point5;
+    public Transform m_point6;
+
     private NavMeshAgent agent;
+    private List<Transform> points; 
+    private int currentPointIndex = 0; 
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        points = new List<Transform> { m_point1, m_point2, m_point3, m_point4, m_point5, m_point6 };
+
+        MoveToNextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = point.position;
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            MoveToNextPoint();
+        }
+    }
+
+    void MoveToNextPoint()
+    {
+        if (points.Count == 0) return;
+
+        agent.destination = points[currentPointIndex].position;
+
+        currentPointIndex = (currentPointIndex + 1) % points.Count;
     }
 }
